8a4714b047b3da802b9ce67326916b7589e5fe73
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-18 22:04:00 +0800

    修复：测模型修复-bug07

--- get.go
+++ get.go
@@ -11,6 +11,9 @@ import (
 func (h *Hub) Get(nsName, key string, client ClientContext) (*Value, error) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
+	if err := h.checkOpen(); err != nil {
+		return nil, err
+	}
 	if err := h.ns.MustExist(nsName); err != nil {
 		return nil, err
 	}
